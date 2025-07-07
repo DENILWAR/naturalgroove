b2945b1493a0645a0b3271ffc92a8c25b192e23c
DENILWAR <manuelfresko7@gmail.com>
2025-07-07 10:04:05 +0200

    Headerblur

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -892,4 +892,13 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    window.addEventListener('scroll', () => {
+        const header = document.querySelector('.header-dynamic');
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+
 });
